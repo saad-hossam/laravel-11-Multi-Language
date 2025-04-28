4cef055ff3e10579795f000c1936eeb103285cc5
saad -hossam <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>
2025-04-28 10:57:29 +0300

    master project

---
+++ lang/en/words.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+
+
+
+    'name_en' => ' English Name',
+    'name_ar' => ' Arabic Name',
+    'name' => '  Name',
+    'actions' => 'Actions',
+
+    'submit' => 'Submit',
+
+];
